2a41b2b732106f89759222d81cf84fd7f489e8cd
ie715536 <ie715536@iteso.mx>
2020-11-13 17:42:42 -0600

    fix problem delay commit

--- WM8731.h
+++ WM8731.h
@@ -23,7 +23,7 @@
 #define I2C0_SDA_PIN 3
 
 /** Max Delay of I2C in ms */
-#define I2C_DELAY 100
+#define I2C_DELAY 400
 /** Data length */
 #define I2C_INIT_DATA_LENGTH 2U
 /** Sub Address Size */
@@ -32,15 +32,16 @@
 
 
 #define WM8731_SLAVE_ADDRESS		0x1A
-#define WM8731_LEFT_LINE_IN			0x00
-#define WM8731_RIGHT_LINE_IN		0x01
+
+#define WM8731_LEFT_LINE_IN			0x00 //
+#define WM8731_RIGHT_LINE_IN		0x01 //
 #define WM8731_LEFT_HEADPHONE_OUT	0x02
 #define WM8731_RIGHT_HEADPHONE_OUT  0x03
-#define WM8731_ANALOGUE_AUDIO		0x04
+#define WM8731_ANALOGUE_AUDIO		0x04 //
 #define WM8731_INTERFACE_FORMAT     0x07
 #define WM8731_SAMPLING_CONTROL     0x08
-#define WM8731_POWER_DOWN_REGISTER	0x0C
-#define WM8731_RESET_REGISTER	    0x0F
+#define WM8731_POWER_DOWN_REGISTER	0x0C //
+#define WM8731_RESET_REGISTER	    0x0F //
 #define WM8731_CODEC_ENABLE			0x12
 
 typedef enum
